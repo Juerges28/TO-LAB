7aae78324b2f029d6e21b06535ee0fdfbf59c2d8
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2024-10-24 23:14:36 -0500

    clase adulto aun no implementada comunicacion

--- Lab4/Resueltos/E4/qtConsole/adulto.h
+++ Lab4/Resueltos/E4/qtConsole/adulto.h
@@ -2,24 +2,22 @@
 #define ADULTO_H
 
 #include <QObject>
+#include <QString>
 #include <QDebug>
-#include "comunicacion.h"  // Incluimos la clase Comunicacion
 
-class adulto : public QObject
-{
+class adulto : public QObject {
     Q_OBJECT
+
+private:
+    QString nombre;
+
 public:
     explicit adulto(QObject *parent = nullptr);
-    ~adulto();
 
     void setNombre(const QString &nomb);
     QString getNombre() const;
 
-    Comunicacion* getComunicacion() const;
-
-private:
-    QString nombre;
-    Comunicacion* comunicacion;  // Cada adulto tiene su objeto Comunicacion
+    ~adulto();
 };
 
 #endif // ADULTO_H
